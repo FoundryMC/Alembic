c96e70cd417b7b80e39200377100a9df6bcb51e2
amo <lunarteclipse@gmail.com>
2023-03-05 17:03:14 +0000

    ✨ AlembicDamageEvents for pre and post damage

---
+++ src/main/java/foundry/alembic/TestEvents.java
@@ -0,0 +1,18 @@
+package foundry.alembic;
+
+import foundry.alembic.event.AlembicDamageEvent;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = Alembic.MODID)
+public class TestEvents {
+    @SubscribeEvent
+    public static void alembicPre(AlembicDamageEvent.Pre event){
+    }
+
+    @SubscribeEvent
+    public static void alembicPost(AlembicDamageEvent.Post event){
+    }
+}
