783cbbb7729577b8f8e0c6f18b964fe7ae163411
dhyces <pokmon987@gmail.com>
2023-03-05 12:03:14 -0500

    Start overrides

--- src/main/java/foundry/alembic/override/AlembicOverride.java
+++ src/main/java/foundry/alembic/override/AlembicOverride.java
@@ -1,11 +1,21 @@
 package foundry.alembic.override;
 
-import foundry.alembic.types.AlembicDamageType;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.StringRepresentable;
+import net.minecraft.world.damagesource.DamageSource;
 
+import javax.annotation.Nonnull;
 import java.util.List;
 
 public class AlembicOverride {
+//    public static final Codec<AlembicOverride> CODEC = RecordCodecBuilder.create(instance ->
+//            instance.group(
+//                    Override.CODEC.fieldOf("")
+//            )
+//    );
+
     private Override override;
     private float percentage;
     private String id;
@@ -54,37 +64,47 @@ public class AlembicOverride {
         return priority;
     }
 
-    public enum Override {
-        ENTITY_TYPE(),
-        DROWN("drown"),
-        FALL("fall"),
-        PRICKED("cactus", "sweetBerryBush"),
-        FIRE("inFire", "onFire", "hotFloor"),
-        LAVA("lava"),
-        SUFFOCATION("inWall"),
-        CRAM("cramming"),
-        STARVE("starve"),
-        IMPACT("flyIntoWall"),
-        OUT_OF_WORLD("outOfWorld"),
-        GENERIC("generic"),
-        MAGIC("magic"),
-        WITHER("wither"),
-        CRUSHED("anvil", "fallingBlock"),
-        DRAGON_BREATH("dragonBreath"),
-        DRIED_OUT("dryout"),
-        FREEZE("freeze"),
-        PIERCED("fallingStalactite","stalagmite"),
-        ATTACK("mob", "player"),
-        MODDED();
+    public enum Override implements StringRepresentable {
+        ENTITY_TYPE("ENTITY_TYPE"),
+        DROWN("DROWN", DamageSource.DROWN),
+        FALL("FALL", DamageSource.FALL),
+        PRICKED("PRICKED", DamageSource.CACTUS, DamageSource.SWEET_BERRY_BUSH),
+        FIRE("FIRE", DamageSource.IN_FIRE, DamageSource.ON_FIRE, DamageSource.HOT_FLOOR),
+        LAVA("LAVA", DamageSource.LAVA),
+        SUFFOCATION("SUFFOCATION", DamageSource.IN_WALL),
+        CRAM("CRAM", DamageSource.CRAMMING),
+        STARVE("STARVE", DamageSource.STARVE),
+        IMPACT("IMPACT", DamageSource.FLY_INTO_WALL),
+        OUT_OF_WORLD("OUT_OF_WORLD", DamageSource.OUT_OF_WORLD),
+        GENERIC("GENERIC", DamageSource.GENERIC),
+        MAGIC("MAGIC", DamageSource.MAGIC),
+        WITHER("WITHER", DamageSource.WITHER),
+        CRUSHED("CRUSHED", DamageSource.ANVIL, DamageSource.FALLING_BLOCK),
+        DRAGON_BREATH("DRAGON_BREATH", DamageSource.DRAGON_BREATH),
+        DRIED_OUT("DRY_OUT", DamageSource.DRY_OUT),
+        FREEZE("FREEZE", DamageSource.FREEZE),
+        PIERCED("PIERCED", DamageSource.FALLING_STALACTITE, DamageSource.STALAGMITE),
+        ATTACK("ATTACK", new DamageSource("mob"), new DamageSource("player")),
+        MODDED("MODDED");
 
-        private final List<String> sources;
+        public static final Codec<Override> CODEC = new EnumCodec<>(values(), Override::valueOf);
 
-        Override(String... sources) {
+        private final String safeName;
+        private final List<DamageSource> sources;
+
+        Override(String safeName, DamageSource... sources) {
+            this.safeName = safeName;
             this.sources = List.of(sources);
         }
 
-        public List<String> getSources() {
+        public List<DamageSource> getSources() {
             return sources;
+        }
+
+        @Nonnull
+        @java.lang.Override
+        public String getSerializedName() {
+            return safeName;
         }
     }
 }
