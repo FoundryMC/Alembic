163ef28ded38e9b3cf97f16ceda9715781358bcc
dhyces <pokmon987@gmail.com>
2023-06-20 22:50:28 -0400

    Migrate to record

--- src/main/java/foundry/alembic/attribute/AttributeHolder.java
+++ src/main/java/foundry/alembic/attribute/AttributeHolder.java
@@ -5,41 +5,15 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 import foundry.alembic.util.CodecUtil;
 import net.minecraft.world.entity.ai.attributes.RangedAttribute;
 
-public class AttributeHolder {
+public record AttributeHolder(RangedAttribute attribute, RangedAttribute shieldingAttribute,
+                              RangedAttribute absorptionAttribute, RangedAttribute resistanceAttribute) {
     public static final Codec<AttributeHolder> CODEC = RecordCodecBuilder.create(instance ->
             instance.group(
-                    CodecUtil.RANGED_ATTRIBUTE_REGISTRY_CODEC.fieldOf("attribute").forGetter(AttributeHolder::getAttribute),
+                    CodecUtil.RANGED_ATTRIBUTE_REGISTRY_CODEC.fieldOf("attribute").forGetter(AttributeHolder::attribute),
                     CodecUtil.RANGED_ATTRIBUTE_REGISTRY_CODEC.fieldOf("shielding_attribute").forGetter(attributeHolder -> attributeHolder.shieldingAttribute),
                     CodecUtil.RANGED_ATTRIBUTE_REGISTRY_CODEC.fieldOf("absorption_attribute").forGetter(attributeHolder -> attributeHolder.absorptionAttribute),
                     CodecUtil.RANGED_ATTRIBUTE_REGISTRY_CODEC.fieldOf("resistance_attribute").forGetter(attributeHolder -> attributeHolder.resistanceAttribute)
             ).apply(instance, AttributeHolder::new)
     );
 
-    private final RangedAttribute attribute;
-    private final RangedAttribute shieldingAttribute;
-    private final RangedAttribute absorptionAttribute;
-    private final RangedAttribute resistanceAttribute;
-
-    public AttributeHolder(RangedAttribute attribute, RangedAttribute shieldingAttribute, RangedAttribute absorptionAttribute, RangedAttribute resistanceAttribute) {
-        this.attribute = attribute;
-        this.shieldingAttribute = shieldingAttribute;
-        this.absorptionAttribute = absorptionAttribute;
-        this.resistanceAttribute = resistanceAttribute;
-    }
-
-    public RangedAttribute getAttribute() {
-        return attribute;
-    }
-
-    public RangedAttribute getShieldingAttribute() {
-        return shieldingAttribute;
-    }
-
-    public RangedAttribute getAbsorptionAttribute() {
-        return absorptionAttribute;
-    }
-
-    public RangedAttribute getResistanceAttribute() {
-        return resistanceAttribute;
-    }
 }
