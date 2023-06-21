d84ca968dea4c9218565fafcbeecc6a4d553d08e
dhyces <pokmon987@gmail.com>
2023-06-20 22:51:30 -0400

    Fix error thrown when entity died before particle was spawned

--- src/main/java/foundry/alembic/compat/TESCompat.java
+++ src/main/java/foundry/alembic/compat/TESCompat.java
@@ -2,25 +2,22 @@ package foundry.alembic.compat;
 
 import com.mojang.math.Vector3f;
 import foundry.alembic.Alembic;
-import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 import net.tslat.tes.api.TESAPI;
 import net.tslat.tes.api.TESParticle;
 import net.tslat.tes.core.particle.type.NumericParticle;
-import net.tslat.tes.core.particle.type.TextParticle;
 import net.tslat.tes.core.state.EntityState;
 
 public class TESCompat {
-    public static ResourceLocation TES_CLAIMANT = Alembic.location("alembic_claimant");
-    public static void spawnParticle(Level level, int entityID, String damageType, float damageAmount, int color){
+    public static final ResourceLocation TES_CLAIMANT = Alembic.location("alembic_claimant");
+    public static void spawnParticle (Level level, int entityID, String damageType, float damageAmount, int color) {
         EntityState state = TESAPI.getTESDataForEntity(entityID);
         LivingEntity e = (LivingEntity) level.getEntity(entityID);
-        if(state != null){
+        if (state != null && e != null) {
             CompoundTag data = new CompoundTag();
             data.putFloat("damage", damageAmount);
             data.putInt("color", color);
@@ -28,7 +25,7 @@ public class TESCompat {
         }
     }
 
-    public static void registerClaimant(){
+    public static void registerClaimant() {
         TESAPI.registerParticleClaimant(TES_CLAIMANT, (entityState, healthDelta, data, particleAdder) -> {
             if(data == null) return healthDelta;
             if(!data.contains("damage") || !data.contains("color")) return healthDelta;
