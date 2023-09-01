3e524f868a0e446a90f2a6c5b7f56a075c60ce0c
dhyces <pokmon987@gmail.com>
2023-09-01 15:53:05 -0400

    Remove DamageSource overload for getDamageType
    Improper usage, there will often be multiple damage types per damage source. To obtain this info, use the OverrideHolder

--- src/main/java/foundry/alembic/types/DamageTypeRegistry.java
+++ src/main/java/foundry/alembic/types/DamageTypeRegistry.java
@@ -31,11 +31,6 @@ public class DamageTypeRegistry {
     }
 
     @Nullable
-    public static AlembicDamageType getDamageType(DamageSource damageSource) {
-        return DAMAGE_TYPES.values().stream().filter(damageType -> damageType.getDamageSource().equals(damageSource)).findFirst().orElse(null);
-    }
-
-    @Nullable
     public static AlembicDamageType getDamageType(Attribute attribute) {
         return DAMAGE_TYPES.values().stream().filter(damageType -> damageType.getAttribute().equals(attribute)).findFirst().orElse(null);
     }
