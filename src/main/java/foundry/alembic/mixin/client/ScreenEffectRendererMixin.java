9a15baa8fa9cedf2c5f65a7baa7d2ce534caa9eb
dhyces <10985914+dhyces@users.noreply.github.com>
2023-12-04 16:00:12 -0500

    Fix first-person screen fire rendering

--- src/main/java/foundry/alembic/mixin/client/ScreenEffectRendererMixin.java
+++ src/main/java/foundry/alembic/mixin/client/ScreenEffectRendererMixin.java
@@ -1,31 +1,32 @@
 package foundry.alembic.mixin.client;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.mojang.blaze3d.systems.RenderSystem;
 import foundry.alembic.caps.AlembicFlammableHandler;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.ScreenEffectRenderer;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.resources.model.Material;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.InventoryMenu;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ScreenEffectRenderer.class)
 public class ScreenEffectRendererMixin {
 
-    @Redirect(method = "renderFire", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderTexture(ILnet/minecraft/resources/ResourceLocation;)V"))
-    private static void alembic$renderFire(int pShaderTexture, ResourceLocation pTextureId) {
+    @WrapOperation(method = "renderFire", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/resources/model/Material;sprite()Lnet/minecraft/client/renderer/texture/TextureAtlasSprite;"))
+    private static TextureAtlasSprite alembic$renderFire(Material instance, Operation<TextureAtlasSprite> original) {
         Player player = Minecraft.getInstance().player;
         if(player == null) {
-            RenderSystem.setShaderTexture(pShaderTexture, pTextureId);
-        } else {
-            player.getCapability(AlembicFlammableHandler.CAPABILITY, null)
-                    .map(alembicFlammable -> alembicFlammable.getTextureLocation(1))
-                    .map(Minecraft.getInstance().getTextureAtlas(InventoryMenu.BLOCK_ATLAS)).ifPresent(textureAtlasSprite -> {
-                        RenderSystem.setShaderTexture(pShaderTexture, textureAtlasSprite.atlasLocation());
-                    });
+            return original.call(instance);
         }
+        return player.getCapability(AlembicFlammableHandler.CAPABILITY, null)
+                .map(alembicFlammable -> alembicFlammable.getTextureLocation(1))
+                .map(Minecraft.getInstance().getTextureAtlas(InventoryMenu.BLOCK_ATLAS))
+                .orElse(original.call(instance));
     }
 
 }
