d8933fcbd87e1428a0e08987e5e4e312d6873abb
dhyces <10985914+dhyces@users.noreply.github.com>
2023-12-09 14:17:48 -0500

    Fix merge error

--- src/main/java/foundry/alembic/stats/item/modifiers/ItemModifier.java
+++ src/main/java/foundry/alembic/stats/item/modifiers/ItemModifier.java
@@ -4,6 +4,10 @@ import com.mojang.serialization.Codec;
 import foundry.alembic.stats.item.ItemModifierType;
 import foundry.alembic.stats.item.ItemStat;
 import foundry.alembic.codecs.CodecUtil;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.UUID;
 
 public sealed interface ItemModifier permits AppendItemModifier, RemoveItemModifier, ReplaceItemModifier {
     Codec<ItemModifier> DISPATCH_CODEC = CodecUtil.safeDispatch(ItemModifierType.CODEC, "type", ItemModifier::getType, ItemModifierType::getCodec);
@@ -12,7 +16,8 @@ public sealed interface ItemModifier permits AppendItemModifier, RemoveItemModif
 
     ItemModifierType getType();
 
-    @Nullable Attribute getAttribute();
+    @Nullable
+    Attribute getAttribute();
 
     @Nullable Attribute getTarget();
 
