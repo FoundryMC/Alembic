02f6aa7c6c005adf1333e11c16a5e68ae9a7cc67
amo <lunarteclipse@gmail.com>
2024-03-31 17:50:01 +0100

    💫 more debug command info in print

--- src/main/java/foundry/alembic/command/AlembicCommand.java
+++ src/main/java/foundry/alembic/command/AlembicCommand.java
@@ -1,5 +1,6 @@
 package foundry.alembic.command;
 
+import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Multimap;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
@@ -16,9 +17,16 @@ import it.unimi.dsi.fastutil.objects.Reference2FloatMap;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.damagesource.DamageType;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import java.io.File;
 import java.io.IOException;
@@ -34,6 +42,11 @@ public class AlembicCommand {
                 .then(Commands.literal("damage_types")
                         .executes((context) -> {
                             Collection<AlembicDamageType> types = DamageTypeManager.getDamageTypes();
+                            List<ResourceLocation> vanillaTypes = context.getSource().getLevel().registryAccess().registry(Registries.DAMAGE_TYPE).get().keySet().stream().toList();
+                            Multimap<String, ResourceLocation> vanillaTypesMap = ArrayListMultimap.create();
+                            for (ResourceLocation type : vanillaTypes) {
+                                vanillaTypesMap.put(type.getNamespace(), type);
+                            }
                             // write all damage types to a file
                             Path writer = null;
                             try {
@@ -44,14 +57,30 @@ public class AlembicCommand {
                                 }
                                 writer = Paths.get("./alembic/damage_types.txt");
                                 List<String> lines = new ArrayList<>();
+                                lines.add("### Alembic Damage Types ###");
                                 for (AlembicDamageType type : types) {
                                     lines.add(type.getId().toString());
+                                }
+                                lines.add("\n");
+                                lines.add("\n");
+                                lines.add("\n");
+                                lines.add("### Vanilla Damage Types ###");
+                                for (Map.Entry<String, Collection<ResourceLocation>> entry : vanillaTypesMap.asMap().entrySet()) {
+                                    lines.add(entry.getKey() + ":");
+                                    for (ResourceLocation type : entry.getValue()) {
+                                        lines.add("  -  " + type.toString());
+                                        AlembicOverride override = OverrideManager.get(context.getSource().getLevel().registryAccess().registry(Registries.DAMAGE_TYPE).get().get(type));
+                                        if (override != null) {
+                                            override.getDamagePercents().forEach((key, value) -> {
+                                                lines.add("    - " + key + " -> " + value);
+                                            });
+                                        }
+                                    }
                                 }
                                 Files.write(writer, lines);
                             } catch (IOException e) {
                                 throw new RuntimeException(e);
                             }
-
                             return 0;
                         }))
                 .then(Commands.literal("overrides")
@@ -88,11 +117,30 @@ public class AlembicCommand {
                                 if (!dir.exists()) {
                                     dir.mkdir();
                                 }
-                                writer = Paths.get("./alembic/resistances.txt");
-                                List<String> lines = overrides.entrySet().stream()
-                                        .sorted(Comparator.comparing(o -> BuiltInRegistries.ENTITY_TYPE.getKey(o.getKey())))
-                                        .map(entry -> entry.getKey().toString() + " -> \n" + statsToString(entry.getValue().getResistances()))
-                                        .toList();
+                                writer = Paths.get("./alembic/entity_resistances.txt");
+                                List<String> lines = new ArrayList<>();
+                                Collection<EntityType<?>> livingEntities = ForgeRegistries.ENTITY_TYPES.getValues();
+                                Multimap<String, EntityType<?>> modEntityTypes = ArrayListMultimap.create();
+                                for (EntityType<?> type : livingEntities) {
+                                    if (type.create(context.getSource().getLevel()) instanceof LivingEntity) {
+                                        modEntityTypes.put(ForgeRegistries.ENTITY_TYPES.getKey(type).getNamespace(), type);
+                                    }
+                                }
+                                for (Map.Entry<String, Collection<EntityType<?>>> entry : modEntityTypes.asMap().entrySet()) {
+                                    lines.add(entry.getKey() + ": ");
+                                    for (EntityType<?> type : entry.getValue()) {
+                                        ResourceLocation id = ForgeRegistries.ENTITY_TYPES.getKey(type);
+                                        AlembicEntityStats stats = StatsManager.get(type);
+                                        StringBuilder builder = new StringBuilder();
+                                        builder.append(id.toString()).append(" -> \n");
+                                        if (stats != null) {
+                                            builder.append(statsToString(stats.getResistances()));
+                                        } else {
+                                            builder.append("No stats found \n");
+                                        }
+                                        lines.add(builder.toString());
+                                    }
+                                }
                                 Files.write(writer, lines);
                             } catch (IOException e) {
                                 throw new RuntimeException(e);
@@ -130,7 +178,7 @@ public class AlembicCommand {
     public static String statsToString(Reference2FloatMap<AlembicDamageType> stats) {
         StringBuilder builder = new StringBuilder();
         for (Reference2FloatMap.Entry<AlembicDamageType> entry : stats.reference2FloatEntrySet()) {
-            builder.append(entry.getKey().getId().toString()).append(" -> ").append(entry.getFloatValue()).append("\n");
+            builder.append("    - ").append(entry.getKey().getId().toString()).append(" -> ").append(entry.getFloatValue()).append("\n");
         }
         return builder.toString();
     }
@@ -138,7 +186,7 @@ public class AlembicCommand {
     public static String formatItemStat(Multimap<EquipmentSlotType, ItemStat> stat) {
         StringBuilder builder = new StringBuilder();
         for (Map.Entry<EquipmentSlotType, ItemStat> entry : stat.entries()) {
-            builder.append(entry.getKey().getName()).append(" -> ").append(entry.getValue().toString()).append("\n");
+            builder.append("    - ").append(entry.getKey().getName()).append(" -> ").append(entry.getValue().toString()).append("\n");
         }
         return builder.toString();
     }
