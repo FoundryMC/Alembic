419969238cadedbf3dbdd83259d0ee954a859bae
dhyces <10985914+dhyces@users.noreply.github.com>
2024-05-06 16:37:00 -0400

    Add comments on tests and add shield test

--- src/test/java/foundry/alembic/tests/BasicDamageTests.java
+++ src/test/java/foundry/alembic/tests/BasicDamageTests.java
@@ -3,13 +3,18 @@ package foundry.alembic.tests;
 import foundry.alembic.Alembic;
 import foundry.alembic.override.OverrideManager;
 import foundry.alembic.tests.mixin.LivingEntityTestAccessor;
+import net.minecraft.commands.arguments.EntityAnchorArgument;
 import net.minecraft.core.BlockPos;
 import net.minecraft.gametest.framework.GameTest;
 import net.minecraft.gametest.framework.GameTestHelper;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.monster.Zombie;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.gametest.GameTestHolder;
 import net.minecraftforge.gametest.PrefixGameTestTemplate;
 import org.apache.commons.lang3.Range;
@@ -18,6 +23,7 @@ import org.apache.commons.lang3.Range;
 @GameTestHolder(Alembic.MODID)
 public class BasicDamageTests {
 
+    // Ensures that a player hurts a zombie within a reasonable and expected damage range.
     @PrefixGameTestTemplate(false)
     @GameTest(template = "9x9x9")
     public void playerHurtZombieTest(final GameTestHelper helper) {
@@ -31,11 +37,15 @@ public class BasicDamageTests {
         helper.succeed();
     }
 
+    // Ensures that I-frames work by hurting a zombie, capturing how much it was hurt by, attempting to hurt it again,
+    // and then effectively comparing the amount of damage to see if it was the same amount of damage (which it should
+    // be if it wasn't hurt again).
     @PrefixGameTestTemplate(false)
     @GameTest(template = "9x9x9")
     public void iFramesTest(final GameTestHelper helper) {
         Player player = helper.makeMockSurvivalPlayer();
         Zombie zombie = helper.spawnWithNoFreeWill(EntityType.ZOMBIE, player.blockPosition().east());
+
         player.attack(zombie);
         float lastHurt = ((LivingEntityTestAccessor)zombie).getLastHurt();
         player.attack(zombie);
@@ -45,6 +55,8 @@ public class BasicDamageTests {
         helper.succeed();
     }
 
+    // There was an issue that resulted in anything that wasn't mapped to an Alembic damage type would not deal damage.
+    // This test uses an unmapped damage type and hurts a zombie with it to make sure that it does work.
     @PrefixGameTestTemplate(false)
     @GameTest(template = "9x9x9")
     public void killTest(final GameTestHelper helper) {
@@ -60,6 +72,29 @@ public class BasicDamageTests {
         helper.succeed();
     }
 
+    // Tests that ShieldStats work correctly. Currently only ensures vanilla-like behavior for physical_damage being fully blocked.
+    @PrefixGameTestTemplate(false)
+    @GameTest(template = "9x9x9")
+    public void shieldTest(final GameTestHelper helper) {
+        Player player = helper.makeMockSurvivalPlayer();
+        Zombie zombie = helper.spawnWithNoFreeWill(EntityType.ZOMBIE, player.blockPosition().east());
+        player.lookAt(EntityAnchorArgument.Anchor.EYES, zombie.getEyePosition());
+
+        float playerHealth = player.getHealth();
+
+        helper.startSequence()
+                .thenExecute(() -> {
+                    player.setItemSlot(EquipmentSlot.MAINHAND, new ItemStack(Items.SHIELD));
+                    player.startUsingItem(InteractionHand.MAIN_HAND);
+                })
+                .thenExecuteAfter(2, () -> zombie.doHurtTarget(player));
+
+        helper.assertTrue(player.getHealth() == playerHealth, "Player took damage while using shield!");
+
+        helper.succeed();
+    }
+
+    // TODO: Finish test. This should test that a custom `test_damage` damage type runs on a zombie being hurt
     @PrefixGameTestTemplate(false)
     @GameTest(template = "9x9x9")
     public void testTestDamage(final GameTestHelper helper) {
@@ -69,7 +104,7 @@ public class BasicDamageTests {
 
         zombie.hurt(source, 1);
 
-        helper.assertTrue(zombie.isDeadOrDying(), "Zombie isn't dying from exceedingly high damage!");
+//        helper.assertTrue(zombie.isDeadOrDying(), "Zombie isn't dying from exceedingly high damage!");
 
         helper.succeed();
     }
